c79668ed2b5d3dc470d39b39431a70812027cd09
Tylor Collins <245583518+cARnage501@users.noreply.github.com>
2026-04-14 03:51:24 -0500

    Delete src directory

--- src/runtime/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod agents;
-pub mod kernel;
-pub mod merge;
-pub mod orchestrator;
-pub mod readouts;
-
-pub use orchestrator::{ExecutionArtifacts, RuntimeRunner};
-pub use readouts::ReadoutEngine;
